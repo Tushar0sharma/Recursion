eaa419d0ef79721a701f91914c94a2f1b423cb78
Tushar0sharma <159058660+Tushar0sharma@users.noreply.github.com>
2024-02-13 19:45:11 +0530

    Update solution.java

--- Subset_Problem/solution.java
+++ Subset_Problem/solution.java
@@ -1,1 +1,39 @@
+import java.util.*;
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int []currentsubset=new int[n];
+        int m=sc.nextInt();
+        int[]cnt={0};
+        subset(arr,n,m,0,currentsubset,0,cnt);
+        System.out.println();
+        System.out.print(cnt[0]);
+    }
+    public static void subset(int[]arr,int n ,int m,int i,int []currentsubset,int subsetsize,int[]cnt){
+        if(i==arr.length){
+            int sum=0;
+            for(int j=0;j<subsetsize;j++){
+                sum+=currentsubset[j];
+            }
+            if(sum==m){
+                for(int k=0;k<subsetsize;k++){
+                    System.out.print(currentsubset[k]+" ");
+                }
+                System.out.print(" ");
+                cnt[0]++;
+            }
+            return;
 
+        }
+        currentsubset[subsetsize]=arr[i];
+        subset(arr,n,m,i+1,currentsubset,subsetsize+1,cnt);
+        subset(arr,n,m,i+1,currentsubset,subsetsize,cnt);
+    }
+}
